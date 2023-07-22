f250de242e6558161120cbe4ff366a5c643df1df
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-22 23:27:38 +0530

    matchcase.py

---
+++ matchcase.py
@@ -0,0 +1,8 @@
+a=1655
+match a:
+    case 1:
+        print("this is Case 1")  # if the case is match the condition the block will be executed other chq next
+    case 16:
+        print("this is Case 16") # if the case is match the condition the block will be executed other chq next
+    case _:
+        print("This is default case like else")  
